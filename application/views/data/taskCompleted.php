b81e701f787c64bf620c3911d93a5d2616138f10
arjunkashyap <arjun.kashyap@yahoo.co.in>
2017-06-28 17:17:42 +0530

    arcihve navigation moved to dynamicPageContainer

--- application/views/data/taskCompleted.php
+++ application/views/data/taskCompleted.php
@@ -1,38 +1,4 @@
 <div class="container">
-    <div class="row first-row">
-        <!-- Column 1 -->
-        <div class="col-md-12 text-center">
-            <ul class="list-inline sub-nav">
-               <li><a href="<?=BASE_URL?>listing/albums/<?=LETTERS?>">Letters</a></li>
-                <li><a>·</a></li>
-                <li><a href="<?=BASE_URL?>listing/albums/<?=ARTICLES?>">Articles</a></li>
-                <li><a>·</a></li>
-                <li><a href="<?=BASE_URL?>listing/collections/<?=BOOKS?>">Books</a></li>
-                <li><a>·</a></li>
-                <li><a href="<?=BASE_URL?>listing/albums/<?=PHOTOGRAPHS?>">Photographs</a></li>
-                <li><a>·</a></li>
-                <li><a href="<?=BASE_URL?>listing/archives/<?=BROCHURES?>__001">Brochures</a></li>
-                <li><a>·</a></li>
-                <li><a href="<?=BASE_URL?>listing/albums/<?=MISCELLANEOUS?>">Miscellaneous</a></li>
-                <li><a>·</a></li>
-                <li><a href="<?=BASE_URL?>listing/albums/<?=UNSORTED?>">Unsorted</a></li>
-                <li><a>·</a></li>
-                <li><a>Search</a></li>
-                <li id="searchForm">
-                    <form class="navbar-form" role="search" action="<?=BASE_URL?>search/field/" method="get">
-                        <div class="input-group add-on">
-                            <input type="text" class="form-control" placeholder="Keywords" name="description" id="description">
-                            <div class="input-group-btn">
-                                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-                            </div>
-                        </div>
-                    </form>
-                </li>
-            </ul>
-        </div>
-    </div>
-</div>
-<div class="container">
     <div class="row gap-above-med">
         <div class="col-md-12">
             <p class="journal-article-title">JSON details for Albums / Archives successfully updated</p>
--- application/views/data/writeerror.php
+++ application/views/data/writeerror.php
@@ -1,38 +1,4 @@
 <div class="container">
-    <div class="row first-row">
-        <!-- Column 1 -->
-        <div class="col-md-12 text-center">
-            <ul class="list-inline sub-nav">
-                <li><a href="<?=BASE_URL?>listing/albums/<?=LETTERS?>">Letters</a></li>
-                <li><a>·</a></li>
-                <li><a href="<?=BASE_URL?>listing/albums/<?=ARTICLES?>">Articles</a></li>
-                <li><a>·</a></li>
-                <li><a href="<?=BASE_URL?>listing/collections/<?=BOOKS?>">Books</a></li>
-                <li><a>·</a></li>
-                <li><a href="<?=BASE_URL?>listing/albums/<?=PHOTOGRAPHS?>">Photographs</a></li>
-                <li><a>·</a></li>
-                <li><a href="<?=BASE_URL?>listing/archives/<?=BROCHURES?>__001">Brochures</a></li>
-                <li><a>·</a></li>
-                <li><a href="<?=BASE_URL?>listing/albums/<?=MISCELLANEOUS?>">Miscellaneous</a></li>
-                <li><a>·</a></li>
-                <li><a href="<?=BASE_URL?>listing/albums/<?=UNSORTED?>">Unsorted</a></li>
-                <li><a>·</a></li>
-                <li><a>Search</a></li>
-                <li id="searchForm">
-                    <form class="navbar-form" role="search" action="<?=BASE_URL?>search/field/" method="get">
-                        <div class="input-group add-on">
-                            <input type="text" class="form-control" placeholder="Keywords" name="description" id="description">
-                            <div class="input-group-btn">
-                                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-                            </div>
-                        </div>
-                    </form>
-                </li>
-            </ul>
-        </div>
-    </div>
-</div>
-<div class="container">
     <div class="row gap-above-med">
         <div class="col-md-12">
             <p class="feedback-title">Problem in writing data to a file</p>
@@ -50,38 +16,6 @@
             <br />
             <br />
             <br />
-        </div>
-    </div>
-</div>
-<div class="container">
-    <div class="row first-row">
-        <!-- Column 1 -->
-        <div class="col-md-12 text-center">
-            <ul class="list-inline sub-nav">
-                <li><a href="<?=BASE_URL?>listing/collections">Photo Collection</a></li>
-                <li><a>·</a></li>
-                <li><a href="#">Publications</a></li>
-                <li><a>·</a></li>
-                <li><a>Search</a></li>
-                <li id="searchForm">
-                    <form class="navbar-form" role="search" action="<?=BASE_URL?>search/field/" method="get">
-                        <div class="input-group add-on">
-                            <input type="text" class="form-control" placeholder="Keywords" name="description" id="description">
-                            <div class="input-group-btn">
-                                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-                            </div>
-                        </div>
-                    </form>
-                </li>
-            </ul>
-        </div>
-    </div>
-</div>
-
-<div class="container">
-    <div class="row gap-above-med">
-        <div class="col-md-12">
-            <p class="feedback-title"><?=$data?></p>
         </div>
     </div>
 </div>
